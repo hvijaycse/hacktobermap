9bb111b914ce49896c9cc668fa2202c88fac128b
Harsh Vijay <harshvijay.cse21@jecrcj.ac.in>
2019-09-30 22:10:06 +0530

    Second commit for hacktoberfest and nice idea

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,11 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Harsh Vijay"
+    location: "Jaipur"
+    latitude :26.958308
+    longitude:75.840289
+
+  },
 ];
